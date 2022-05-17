eba6cc0504c5200f3836fb17fe9ed10918ca4611
Clayton <claytongorham@gmail.com>
2022-05-17 11:15:09 -0700

    animation changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,6 +23,7 @@ export default function Home() {
     paddingTop: "1vw",
     flexWrap: "wrap",
     
+    
   
   }
 
@@ -30,19 +31,21 @@ export default function Home() {
    paddingTop: "2vw",
    paddingLeft: "1vw",
    justifyContent: "flex-start",
-   animation: "fadeSpin .5s",
+   animation: "fadeSpin 1s",
   }
 
  
 
   return (
     <div id='home' style={wrapperStyle}>
-
+      <div id='test'>
     <h1 id='full' style={h1}>Full <br></br>Stack<br></br>Software<br></br>Developer</h1> 
-    
+    <div id='test1'></div>
+    </div>
     <div style={computerimg}>
       <img alt="old Macintosh computer" src="mccomputer2.png" width="380px" ></img>
     </div>
+    
 
       
 </div>
